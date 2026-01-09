fa38553078efb808415653bd0290b3632562d042
janbridley <bradleyjenj@gmail.com>
2026-01-09 10:35:09 -0500

    Update to work with sets of vectors

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from hypothesis import given
+from hypothesis import given, settings
 from hypothesis import strategies as st
 from hypothesis.extra import numpy as np_strategies
 
@@ -27,6 +27,7 @@ def test_stable_normalize_zero_vector(shape):
     np.testing.assert_array_equal(result, vec)
 
 
+@settings(max_examples=1000)
 @given(
     np_strategies.arrays(
         dtype=np.float64,
@@ -41,3 +42,24 @@ def test_stable_normalize_3d_vector(vec):
         np.testing.assert_allclose(np.linalg.norm(result), 1.0, atol=5e-16)
     else:
         np.testing.assert_array_equal(result, np.zeros_like(vec))
+
+
+@settings(max_examples=1000)
+@given(
+    np_strategies.arrays(
+        dtype=np.float64,
+        shape=np_strategies.array_shapes(min_dims=2, max_dims=2, min_side=1).map(
+            lambda s: (s[0], 3)
+        ),
+        elements=st.floats(allow_nan=False, allow_infinity=False),
+    )
+)
+def test_stable_normalize_n3_vector(vecs):
+    """Result should have unit length for each non-zero row in an array."""
+    result = _stable_normalize(vecs)
+    norms = np.linalg.norm(result, axis=-1)
+    input_max = np.max(np.abs(vecs), axis=-1)
+
+    is_nonzero = input_max > 0
+    np.testing.assert_allclose(norms[is_nonzero], 1.0, atol=5e-16)
+    np.testing.assert_array_equal(result[~is_nonzero], 0.0)
